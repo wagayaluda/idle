80ce2f44b74dbe3f96ef5fbf9d46a801de50827b
pathC <wegood9@users.noreply.github.com>
2018-04-14 21:00:09 +0800

    use https://steamcommunity.com

--- idle_master-master/Source/IdleMaster/frmBrowser.cs
+++ idle_master-master/Source/IdleMaster/frmBrowser.cs
@@ -34,9 +34,9 @@ namespace IdleMaster
       lblSaving.Text = localization.strings.saving_info;
 
       // Delete Steam cookie data from the browser control
-      InternetSetCookie("http://steamcommunity.com", "sessionid", ";expires=Mon, 01 Jan 0001 00:00:00 GMT");
-      InternetSetCookie("http://steamcommunity.com", "steamLogin", ";expires=Mon, 01 Jan 0001 00:00:00 GMT");
-      InternetSetCookie("http://steamcommunity.com", "steamRememberLogin", ";expires=Mon, 01 Jan 0001 00:00:00 GMT");
+      InternetSetCookie("https://steamcommunity.com", "sessionid", ";expires=Mon, 01 Jan 0001 00:00:00 GMT");
+      InternetSetCookie("https://steamcommunity.com", "steamLogin", ";expires=Mon, 01 Jan 0001 00:00:00 GMT");
+      InternetSetCookie("https://steamcommunity.com", "steamRememberLogin", ";expires=Mon, 01 Jan 0001 00:00:00 GMT");
 
       // When the form is loaded, navigate to the Steam login page using the web browser control
       wbAuth.Navigate("https://steamcommunity.com/login/home/?goto=my/profile", "_self", null, "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko");
